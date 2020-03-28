3d184c8d2e402e50dc94fbde8d635ecee8413191
913062247@qq.com <123456789>
2020-03-28 12:01:48 +0800

    code

---
+++ mq-consumer/src/main/java/com/cyz/mq/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.cyz.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:91306
+ * @CreateTime:2020/3/25 22:27
+ * @Description:
+ **/
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class,args);
+    }
+}
